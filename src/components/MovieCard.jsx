0b32161ccf66b5536ad87c140852efbf4e56f4ec
andreamaluta <andreamaluta@gmail.com>
2025-05-30 16:37:43 +0200

    [UPD] made Movie Card component

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const MovieCard = ({ movie }) => {
+    const { id, titolo, regista, genere, descrizione, immagine } = movie
+    return (
+        <>
+            <div className="col-12 col-md-6 col-lg-4 g-3" key={id}>
+                <div className="card">
+                    <div className="card-img-top">
+                        <img src={immagine} alt="" className='img-fluid' />
+                    </div>
+                    <div className="card-body">
+                        <h3>{titolo}</h3>
+                        <h4>{regista}</h4>
+                        <p>{descrizione}</p>
+                        <Link className='btn btn-primary' to='/movies/1'>Leggi di più</Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default MovieCard
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useState } from 'react';
+import MovieCard from '../components/MovieCard';
 
 const initialMovies = [
     {
@@ -59,21 +60,9 @@ const HomePage = () => {
             </h2>
             <div className="row">
                 {movies.map((movie) => {
-                    const { id, titolo, regista, genere, descrizione, immagine } = movie
+
                     return (
-                        <div className="col-12 col-md-6 col-lg-4 g-3" key={id}>
-                            <div className="card">
-                                <div className="card-img-top">
-                                    <img src={immagine} alt="" className='img-fluid' />
-                                </div>
-                                <div className="card-body">
-                                    <h3>{titolo}</h3>
-                                    <h4>{regista}</h4>
-                                    <p>{descrizione}</p>
-                                    <Link className='btn btn-primary' to='/movies/1'>Leggi di più</Link>
-                                </div>
-                            </div>
-                        </div>
+                        <MovieCard movie={movie} key={movie.id} />
                     )
                 })}
 
