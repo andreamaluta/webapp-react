f246aa7e301f54a006f3e2a4008b8bb6ab209a53
andreamaluta <andreamaluta@gmail.com>
2025-05-30 14:48:50 +0200

    [UPD] completed routes tree

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
         <Routes>
           <Route element={<DefaultLayout />}>
             <Route index element={<h1>Homepage</h1>}></Route>
+            <Route path='/movies/:id' element={<h1>Dettaglio pagina</h1>}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
