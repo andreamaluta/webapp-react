77d73b4d341ea6ac19563e0f7a226acfb6b31c8c
andreamaluta <andreamaluta@gmail.com>
2025-05-30 15:30:19 +0200

    [UPD] create the static card

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,6 +7,86 @@ const HomePage = () => {
             <h2>
                 <i>Lista di film</i>
             </h2>
+            <div className="row">
+                <div className="col-12">
+                    <div className="card">
+                        <div className="card-img-top">
+                            <img src="https://picsum.photos/500/300" alt="" />
+                        </div>
+                        <div className="card-body">
+                            <h3>Titolo</h3>
+                            <h4>Autore</h4>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero voluptates quidem assumenda asperiores, velit dignissimos voluptas recusandae natus nisi praesentium, est aliquid fuga. Molestias ratione quod distinctio, consequatur unde sint.</p>
+                            <a href="#" className='text-decoration-none'>Cerca</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12">
+                    <div className="card">
+                        <div className="card-img-top">
+                            <img src="https://picsum.photos/500/300" alt="" />
+                        </div>
+                        <div className="card-body">
+                            <h3>Titolo</h3>
+                            <h4>Autore</h4>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero voluptates quidem assumenda asperiores, velit dignissimos voluptas recusandae natus nisi praesentium, est aliquid fuga. Molestias ratione quod distinctio, consequatur unde sint.</p>
+                            <a href="#" className='text-decoration-none'>Cerca</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12">
+                    <div className="card">
+                        <div className="card-img-top">
+                            <img src="https://picsum.photos/500/300" alt="" />
+                        </div>
+                        <div className="card-body">
+                            <h3>Titolo</h3>
+                            <h4>Autore</h4>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero voluptates quidem assumenda asperiores, velit dignissimos voluptas recusandae natus nisi praesentium, est aliquid fuga. Molestias ratione quod distinctio, consequatur unde sint.</p>
+                            <a href="#" className='text-decoration-none'>Cerca</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12">
+                    <div className="card">
+                        <div className="card-img-top">
+                            <img src="https://picsum.photos/500/300" alt="" />
+                        </div>
+                        <div className="card-body">
+                            <h3>Titolo</h3>
+                            <h4>Autore</h4>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero voluptates quidem assumenda asperiores, velit dignissimos voluptas recusandae natus nisi praesentium, est aliquid fuga. Molestias ratione quod distinctio, consequatur unde sint.</p>
+                            <a href="#" className='text-decoration-none'>Cerca</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12">
+                    <div className="card">
+                        <div className="card-img-top">
+                            <img src="https://picsum.photos/500/300" alt="" />
+                        </div>
+                        <div className="card-body">
+                            <h3>Titolo</h3>
+                            <h4>Autore</h4>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero voluptates quidem assumenda asperiores, velit dignissimos voluptas recusandae natus nisi praesentium, est aliquid fuga. Molestias ratione quod distinctio, consequatur unde sint.</p>
+                            <a href="#" className='text-decoration-none'>Cerca</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12">
+                    <div className="card">
+                        <div className="card-img-top">
+                            <img src="https://picsum.photos/500/300" alt="" />
+                        </div>
+                        <div className="card-body">
+                            <h3>Titolo</h3>
+                            <h4>Autore</h4>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero voluptates quidem assumenda asperiores, velit dignissimos voluptas recusandae natus nisi praesentium, est aliquid fuga. Molestias ratione quod distinctio, consequatur unde sint.</p>
+                            <a href="#" className='text-decoration-none'>Cerca</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
