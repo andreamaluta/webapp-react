66058331172a532ae1fa64fc82eda3fde96cb0a6
andreamaluta <andreamaluta@gmail.com>
2025-05-30 15:50:08 +0200

    [UPD] modify the movie page

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 const MoviePage = () => {
     return (
         <>
-            <div className="row d-flex flex-column">
-                <div className="col-12 col-md-6 col-lg-4 my-4">Immagine</div>
-                <div className="col-12 col-md-6 col-lg-4">
-                    <h1>Titolo</h1>
-                    <p>informazioni</p>
+            <div className="row">
+                <div className="col-12 col-md-6 col-lg-4 my-4 d-flex">
+                    <img src="https://picsum.photos/500/300" alt="" />
+                    <div className='mx-3'>
+                        <h1>Titolo</h1>
+                        <p>informazioni</p>
+                    </div>
                 </div>
             </div>
         </>
