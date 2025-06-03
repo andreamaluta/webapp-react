cf5ba9047ea8cd7de8d6267d72bb5450cb9a2304
andreamaluta <andreamaluta@gmail.com>
2025-06-03 15:22:37 +0200

    [UPD] linked the axios call to movies variables

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,19 +2,18 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const MovieCard = ({ movie }) => {
-    const { id, titolo, regista, genere, descrizione, immagine } = movie
     return (
         <>
-            <div className="col-12 col-md-6 col-lg-4 g-3" key={id}>
+            <div className="col-12 col-md-6 col-lg-4 g-3" key={movie.id}>
                 <div className="card">
                     <div className="card-img-top">
-                        <img src={immagine} alt="" className='img-fluid' />
+                        <img src={movie.image} alt="" className='img-fluid' />
                     </div>
                     <div className="card-body">
-                        <h3>{titolo}</h3>
-                        <h4>{regista}</h4>
-                        <p>{descrizione}</p>
-                        <Link className='btn btn-primary' to={`/movies/${id}`}>Leggi di più</Link>
+                        <h3>{movie.title}</h3>
+                        <h4>{movie.director}</h4>
+                        <p>{movie.abstract}</p>
+                        <Link className='btn btn-primary' to={`/movies/${movie.id}`}>Leggi di più</Link>
                     </div>
                 </div>
             </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,62 +4,17 @@ import { useState } from 'react';
 import MovieCard from '../components/MovieCard';
 import axios from 'axios';
 
-const initialMovies = [
-    {
-        id: 1,
-        titolo: "Il Padrino",
-        regista: "Francis Ford Coppola",
-        genere: "Drammatico / Crime",
-        descrizione: "La saga della famiglia mafiosa Corleone nell'America del dopoguerra.",
-        immagine: "https://picsum.photos/500/300"
-    },
-    {
-        id: 2,
-        titolo: "Inception",
-        regista: "Christopher Nolan",
-        genere: "Sci-Fi / Thriller",
-        descrizione: "Un ladro che ruba segreti attraverso i sogni deve compiere un'azione impossibile: l'inception.",
-        immagine: "https://picsum.photos/500/300"
-    },
-    {
-        id: 3,
-        titolo: "La vita è bella",
-        regista: "Roberto Benigni",
-        genere: "Drammatico / Commedia",
-        descrizione: "Un padre usa l'immaginazione per proteggere il figlio dagli orrori dell'Olocausto.",
-        immagine: "https://picsum.photos/500/300"
-    },
-    {
-        id: 4,
-        titolo: "Pulp Fiction",
-        regista: "Quentin Tarantino",
-        genere: "Crime / Drammatico",
-        descrizione: "Storie intrecciate di criminali, pugili e gangster a Los Angeles.",
-        immagine: "https://picsum.photos/500/300"
-    },
-    {
-        id: 5,
-        titolo: "Il Signore degli Anelli: La Compagnia dell'Anello",
-        regista: "Peter Jackson",
-        genere: "Fantasy / Avventura",
-        descrizione: "Un giovane hobbit intraprende un viaggio per distruggere un potente anello.",
-        immagine: "https://picsum.photos/500/300"
-    }
-];
-
-
-
 const HomePage = () => {
 
-    const [movies, setMovies] = useState(initialMovies)
+    const [movies, setMovies] = useState([])
 
     useEffect(() => {
         axios.get('http://127.0.0.1:3000/films/').then((resp) => {
-            console.log(resp.data);
+            setMovies(resp.data);
         }).catch((err) => {
             console.log(err);
         })
-    })
+    }, [])
 
     return (
         <>
